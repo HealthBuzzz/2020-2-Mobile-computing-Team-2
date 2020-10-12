02304e614cc813dd17cdfd4292c9b2bb453d6d3b
KYHSGeekCode <jourhyang123@gmail.com>
2020-10-12 22:35:15 +0900

    :fire: Clean code

--- app/src/main/java/com/healthbuzz/healthbuzz/ItemDetailActivity.kt
+++ app/src/main/java/com/healthbuzz/healthbuzz/ItemDetailActivity.kt
@@ -1,18 +1,11 @@
 package com.healthbuzz.healthbuzz
 
-import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import android.view.MenuItem
 
-/**
- * An activity representing a single Item detail screen. This
- * activity is only used on narrow width devices. On tablet-size devices,
- * item details are presented side-by-side with a list of items
- * in a [ItemListActivity].
- */
 class ItemDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +15,7 @@ class ItemDetailActivity : AppCompatActivity() {
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own detail action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
 
         // Show the Up button in the action bar.
@@ -40,33 +33,26 @@ class ItemDetailActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             // Create the detail fragment and add it to the activity
             // using a fragment transaction.
-            val fragment = ItemDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ItemDetailFragment.ARG_ITEM_ID,
-                            intent.getStringExtra(ItemDetailFragment.ARG_ITEM_ID))
-                }
-            }
-
+            val fragment = ItemDetailFragment()
             supportFragmentManager.beginTransaction()
-                    .add(R.id.item_detail_container, fragment)
-                    .commit()
+                .add(R.id.item_detail_container, fragment)
+                .commit()
         }
     }
 
     override fun onOptionsItemSelected(item: MenuItem) =
-            when (item.itemId) {
-                android.R.id.home -> {
-
-                    // This ID represents the Home or Up button. In the case of this
-                    // activity, the Up button is shown. For
-                    // more details, see the Navigation pattern on Android Design:
-                    //
-                    // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+        when (item.itemId) {
+            android.R.id.home -> {
+                // This ID represents the Home or Up button. In the case of this
+                // activity, the Up button is shown. For
+                // more details, see the Navigation pattern on Android Design:
+                //
+                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
 
 //                    navigateUpTo(Intent(this, ItemListActivity::class.java))
 
-                    true
-                }
-                else -> super.onOptionsItemSelected(item)
+                true
             }
+            else -> super.onOptionsItemSelected(item)
+        }
 }
--- app/src/main/java/com/healthbuzz/healthbuzz/ItemDetailFragment.kt
+++ app/src/main/java/com/healthbuzz/healthbuzz/ItemDetailFragment.kt
@@ -8,29 +8,12 @@ import androidx.fragment.app.Fragment
 
 /**
  * A fragment representing a single Item detail screen.
- * This fragment is either contained in a [ItemListActivity]
- * in two-pane mode (on tablets) or a [ItemDetailActivity]
+ * This fragment is contained in a [ItemDetailActivity]
  * on handsets.
  */
 class ItemDetailFragment : Fragment() {
-
-    /**
-     * The dummy content this fragment is presenting.
-     */
-//    private var item: DummyContent.DummyItem? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-//        arguments?.let {
-////            if (it.containsKey(ARG_ITEM_ID)) {
-////                // Load the dummy content specified by the fragment
-////                // arguments. In a real-world scenario, use a Loader
-////                // to load content from a content provider.
-//////                item = DummyContent.ITEM_MAP[it.getString(ARG_ITEM_ID)]
-//////                activity?.findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout)?.title = item?.content
-////            }
-//        }
     }
 
     override fun onCreateView(
@@ -38,20 +21,6 @@ class ItemDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.item_detail, container, false)
-
-        // Show the dummy content as text in a TextView.
-//        item?.let {
-//            rootView.findViewById<TextView>(R.id.item_detail).text = it.details
-//        }
-
         return rootView
-    }
-
-    companion object {
-        /**
-         * The fragment argument representing the item ID that this fragment
-         * represents.
-         */
-        const val ARG_ITEM_ID = "item_id"
     }
 }
