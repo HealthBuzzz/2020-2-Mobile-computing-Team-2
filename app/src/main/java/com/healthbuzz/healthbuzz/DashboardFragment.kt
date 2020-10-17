fda638b027a9e11c95b4326060e7c0c0bc46792c
KYHSGeekCode <jourhyang123@gmail.com>
2020-10-17 22:21:30 +0900

    :lipstick: Different image for different cards

--- app/src/main/java/com/healthbuzz/healthbuzz/DashboardFragment.kt
+++ app/src/main/java/com/healthbuzz/healthbuzz/DashboardFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_dashboard.view.*
 
 /**
  * A fragment representing a single Item detail screen.
@@ -21,6 +24,14 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val stretchingDrawable = ResourcesCompat.getDrawable(resources, R.drawable.stretching, null)
+        val waterDrawable = ResourcesCompat.getDrawable(resources, R.drawable.drink_water, null)
+        rootView.cardview_layout_stretching.findViewById<ImageView>(R.id.ivCardImage)
+            .setImageDrawable(stretchingDrawable)
+        rootView.cardview_layout_water.findViewById<ImageView>(R.id.ivCardImage)
+            .setImageDrawable(waterDrawable)
+
+
         return rootView
     }
 }
