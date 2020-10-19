3107302cbe5c34feb451b1ba86d1be59aa1addc2
KYHSGeekCode <jourhyang123@gmail.com>
2020-10-19 22:43:34 +0900

    :recycle: Extract StartSensorService()

--- app/src/main/java/com/healthbuzz/healthbuzz/BootReceiver.kt
+++ app/src/main/java/com/healthbuzz/healthbuzz/BootReceiver.kt
@@ -3,7 +3,6 @@ package com.healthbuzz.healthbuzz
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.util.Log
 
 
@@ -11,12 +10,7 @@ class BootReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        val intent = Intent(context, SensorService::class.java)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            context.startForegroundService(intent)
-        } else {
-            context.startService(intent)
-        }
+        startSensorService(context)
         Log.i(TAG, "started")
     }
 }
--- app/src/main/java/com/healthbuzz/healthbuzz/Util.kt
+++ app/src/main/java/com/healthbuzz/healthbuzz/Util.kt
@@ -1,5 +1,9 @@
 package com.healthbuzz.healthbuzz
 
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+
 
 // Generates log TAG constant automatically
 val Any.TAG: String
@@ -7,3 +11,12 @@ val Any.TAG: String
         val tag = javaClass.simpleName
         return if (tag.length <= 23) tag else tag.substring(0, 23)
     }
+
+fun startSensorService(context: Context) {
+    val intent = Intent(context, SensorService::class.java)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        context.startForegroundService(intent)
+    } else {
+        context.startService(intent)
+    }
+}
