74c6d98683cfe8e1b9ba373727df023b8a6a9f33
KYHSGeekCode <jourhyang123@gmail.com>
2020-11-01 19:11:27 +0900

    :bug: Fix #49 ; check service already running

--- app/src/main/java/com/healthbuzz/healthbuzz/Util.kt
+++ app/src/main/java/com/healthbuzz/healthbuzz/Util.kt
@@ -1,8 +1,10 @@
 package com.healthbuzz.healthbuzz
 
+import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.util.Log
 
 
 // Generates log TAG constant automatically
@@ -13,10 +15,27 @@ val Any.TAG: String
     }
 
 fun startSensorService(context: Context) {
+    if (!isServiceRunning(context, "com.healthbuzz.healthbuzz.SensorService"))
+        startSensorServiceSub(context)
+}
+
+fun startSensorServiceSub(context: Context) {
     val intent = Intent(context, SensorService::class.java)
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
         context.startForegroundService(intent)
     } else {
         context.startService(intent)
     }
+}
+
+// Though deprecated but will work as expected
+fun isServiceRunning(context: Context, className: String): Boolean {
+    val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager?
+    val info = activityManager!!.getRunningServices(Int.MAX_VALUE)
+    if (info == null || info.size == 0) return false
+    for (aInfo in info) {
+        Log.d("IsServiceRunnig", "classname: ${aInfo.service.className}")
+        if (className == aInfo.service.className) return true
+    }
+    return false
 }
