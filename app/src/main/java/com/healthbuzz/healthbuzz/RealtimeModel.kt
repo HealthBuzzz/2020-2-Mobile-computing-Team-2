b74ccf3f3134143b9da6550e953c2f2f09c4e380
KYHSGeekCode <jourhyang123@gmail.com>
2020-11-01 09:20:58 +0900

    :bug: Add default value for livedata-s

--- app/src/main/java/com/healthbuzz/healthbuzz/RealtimeModel.kt
+++ app/src/main/java/com/healthbuzz/healthbuzz/RealtimeModel.kt
@@ -3,8 +3,16 @@ package com.healthbuzz.healthbuzz
 import androidx.lifecycle.MutableLiveData
 
 object RealtimeModel {
-    val stretching_time_left = MutableLiveData<Long>()
-    val water_time_left = MutableLiveData<Long>()
-    val stretching_count = MutableLiveData<Long>()
-    val water_count = MutableLiveData<Long>()
+    val stretching_time_left = MutableLiveData<Long>().also {
+        it.value = 9999
+    }
+    val water_time_left = MutableLiveData<Long>().also {
+        it.value = 9999
+    }
+    val stretching_count = MutableLiveData<Long>().also {
+        it.value = 0
+    }
+    val water_count = MutableLiveData<Long>().also {
+        it.value = 0
+    }
 }
