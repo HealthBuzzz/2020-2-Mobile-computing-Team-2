482ceed75a6f5f90584aa198142c259c0fc4b042
Yasmine <yasaman.abtahi74@gmail.com>
2020-11-09 16:08:03 +0900

    Add wear module. Same package

---
+++ wear/src/main/java/com/healthbuzz/healthbuzz/BlActivity.java
@@ -0,0 +1,132 @@
+package com.healthbuzz.healthbuzz;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.Wearable;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class BlActivity extends WearableActivity {
+
+    private TextView textView;
+    Button talkButton;
+    int receivedMessageNumber = 1;
+    int sentMessageNumber = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bl);
+        textView =  findViewById(R.id.text);
+        talkButton =  findViewById(R.id.talkClick);
+
+//Create an OnClickListener//
+
+        talkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String onClickMessage = "coyot in the house..." + sentMessageNumber++;
+                textView.setText(onClickMessage);
+
+//Use the same path//
+
+                String datapath = "/my_path";
+                new SendMessage(datapath, onClickMessage).start();
+
+            }
+        });
+
+//Register to receive local broadcasts, which we'll be creating in the next step//
+
+        IntentFilter newFilter = new IntentFilter(Intent.ACTION_SEND);
+        Receiver messageReceiver = new Receiver();
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver, newFilter);
+
+    }
+
+    public class Receiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+//Display the following when a new message is received//
+
+            String onMessageReceived = intent.getStringExtra("message"); //"I just received a message from the handheld " + receivedMessageNumber++;
+            textView.setText(onMessageReceived);
+
+        }
+    }
+
+    class SendMessage extends Thread {
+        String path;
+        String message;
+
+//Constructor for sending information to the Data Layer//
+
+        SendMessage(String p, String m) {
+            path = p;
+            message = m;
+        }
+
+        public void run() {
+
+//Retrieve the connected devices//
+
+            Task<List<Node>> nodeListTask =
+                    Wearable.getNodeClient(getApplicationContext()).getConnectedNodes();
+            try {
+
+//Block on a task and get the result synchronously//
+
+                List<Node> nodes = Tasks.await(nodeListTask);
+                for (Node node : nodes) {
+
+//Send the message///
+
+                    Task<Integer> sendMessageTask =
+                            Wearable.getMessageClient(com.healthbuzz.healthbuzz.BlActivity.this).sendMessage(node.getId(), path, message.getBytes());
+
+                    try {
+
+                        Integer result = Tasks.await(sendMessageTask);
+
+//Handle the errors//
+
+                    } catch (ExecutionException exception) {
+
+//TO DO//
+
+                    } catch (InterruptedException exception) {
+
+//TO DO//
+
+                    }
+
+                }
+
+            } catch (ExecutionException exception) {
+
+//TO DO//
+
+            } catch (InterruptedException exception) {
+
+//TO DO//
+
+            }
+        }
+    }
+}
---
+++ wear/src/main/java/com/healthbuzz/healthbuzz/BlService.java
@@ -0,0 +1,36 @@
+package com.healthbuzz.healthbuzz;
+
+import android.content.Intent;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class BlService extends WearableListenerService {
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+
+//If the message’s path equals "/my_path"...//
+
+        if (messageEvent.getPath().equals("/my_path")) {
+
+//...retrieve the message//
+
+            final String message = new String(messageEvent.getData());
+            Intent messageIntent = new Intent();
+            messageIntent.setAction(Intent.ACTION_SEND);
+            messageIntent.putExtra("message", message);
+
+//Broadcast the received Data Layer messages locally//
+
+            LocalBroadcastManager.getInstance(this).sendBroadcast(messageIntent);
+        }
+        else {
+            super.onMessageReceived(messageEvent);
+        }
+    }
+
+}
+
