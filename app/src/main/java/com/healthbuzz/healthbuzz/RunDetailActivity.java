356a4d1303bae196f20ba0b0fc92cbf0c929d03e
KYHSGeekCode <jourhyang123@gmail.com>
2020-11-15 20:44:14 +0900

    :sparkles: Run detail activity

---
+++ app/src/main/java/com/healthbuzz/healthbuzz/RunDetailActivity.java
@@ -0,0 +1,14 @@
+package com.healthbuzz.healthbuzz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RunDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_run_detail);
+    }
+}
