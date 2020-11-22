2afcfd1d035d830ab02d7bbd469016d2430634d2
sugsugsug <thddnrud2010@gmail.com>
2020-11-22 23:56:21 +0900

    add badge changing

--- app/src/main/java/com/healthbuzz/healthbuzz/WelcomeFragment.java
+++ app/src/main/java/com/healthbuzz/healthbuzz/WelcomeFragment.java
@@ -8,6 +8,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -54,6 +55,52 @@ public class WelcomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        ImageView manView = getView().findViewById(R.id.man);
+        ImageView treeView = getView().findViewById(R.id.tree);
+        RealtimeModel.INSTANCE.getStretching_count().observe(getViewLifecycleOwner(), stretching_count -> {
+            int int_count = stretching_count.intValue();
+            if (int_count >= 6) {
+                int_count = 5;
+            }
+            switch (int_count) {
+                case 0:
+                case 1:
+                    manView.setImageResource(R.drawable.man1);
+                    break;
+                case 2:
+                case 3:
+                    manView.setImageResource(R.drawable.man2);
+                    break;
+                case 4:
+                case 5:
+                    manView.setImageResource(R.drawable.man3);
+                    break;
+                default:
+                    throw new IllegalStateException();
+            }
+        });
+        RealtimeModel.INSTANCE.getWater_count().observe(getViewLifecycleOwner(), water_count -> {
+            int int_count = water_count.intValue();
+            int_count /= 1000;
+            if (int_count >= 3) {
+                int_count = 2;
+            }
+            switch (int_count) {
+                case 0:
+                    treeView.setImageResource(R.drawable.tree1);
+                    break;
+                case 1:
+                    treeView.setImageResource(R.drawable.tree2);
+                    break;
+                case 2:
+                    treeView.setImageResource(R.drawable.tree3);
+                    break;
+                default:
+                    throw new IllegalStateException();
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_welcome, container, false);
     }
 
