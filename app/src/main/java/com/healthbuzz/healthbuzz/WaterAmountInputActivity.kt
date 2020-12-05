f8ae1b44b4e87040553bf050233250a7803480f0
KYHSGeekCode <jourhyang123@gmail.com>
2020-12-06 08:48:48 +0900

    :sparkles: Water input activitiy

---
+++ app/src/main/java/com/healthbuzz/healthbuzz/WaterAmountInputActivity.kt
@@ -0,0 +1,11 @@
+package com.healthbuzz.healthbuzz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WaterAmountInputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_water_amount_input)
+    }
+}
