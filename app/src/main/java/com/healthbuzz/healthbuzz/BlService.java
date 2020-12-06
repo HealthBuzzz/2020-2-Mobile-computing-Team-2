84bbc4e0eebbaaa4e8089c45c814d5151813c98b
Yasmine <yasaman.abtahi74@gmail.com>
2020-12-06 11:46:52 +0900

    Make sure BL is continous

--- app/src/main/java/com/healthbuzz/healthbuzz/BlService.java
+++ app/src/main/java/com/healthbuzz/healthbuzz/BlService.java
@@ -8,11 +8,17 @@ import android.widget.Toast;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.preference.PreferenceManager;
 
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.Wearable;
 import com.google.android.gms.wearable.WearableListenerService;
 
 import java.util.LinkedList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 //Extend WearableListenerService//
 
@@ -20,6 +26,19 @@ public class BlService extends WearableListenerService {
 
     public static LinkedList<String[]> output = new LinkedList<>();
 
+
+    @Override
+    public void onDestroy() {
+        Log.e("CYT_LOG" , "destroyed...");
+        SharedPreferences sharedPrefs =  PreferenceManager.getDefaultSharedPreferences(this);
+
+        boolean continueService = sharedPrefs.getBoolean("sync2", false);
+        if(continueService) {
+            String datapath = "/my_path";
+            new SendMessage(datapath, "_startrealtime").start();
+        }
+        super.onDestroy();
+    }
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
 
@@ -57,6 +76,43 @@ public class BlService extends WearableListenerService {
             LocalBroadcastManager.getInstance(this).sendBroadcast(messageIntent);
         } else {
             super.onMessageReceived(messageEvent);
+        }
+    }
+
+    class SendMessage extends Thread {
+        String path;
+        String message;
+
+        //Constructor for sending information to the Data Layer//
+        SendMessage(String p, String m) {
+            path = p;
+            message = m;
+        }
+        public void run() {
+            //Retrieve the connected devices//
+            Task<List<Node>> nodeListTask =
+                    Wearable.getNodeClient(getApplicationContext()).getConnectedNodes();
+            try {
+                //Block on a task and get the result synchronously//
+                List<Node> nodes = Tasks.await(nodeListTask);
+                for (Node node : nodes) {
+                    //Send the message///
+                    Task<Integer> sendMessageTask =
+                            Wearable.getMessageClient(BlService.this).sendMessage(node.getId(), path, message.getBytes());
+                    try {
+                        Integer result = Tasks.await(sendMessageTask);
+                        //Handle the errors//
+                    } catch (ExecutionException exception) {
+                        //TO DO//
+                    } catch (InterruptedException exception) {
+                        //TO DO//
+                    }
+                }
+            } catch (ExecutionException exception) {
+                //TO DO//
+            } catch (InterruptedException exception) {
+                //TO DO//
+            }
         }
     }
 
